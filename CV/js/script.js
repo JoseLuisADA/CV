3f7da73f2420f5c75ef29b2dda3a6af041d4e80d
Jose Luis Luzuriaga Alanis <joseluis.luzuriagaalanis.web@adaits.es>
2024-03-05 14:31:55 +0100

    ACTUALIZACIÓN CURRICULUM

--- CV/js/script.js
+++ CV/js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log("Documento cargado");
+
+    initializeThemeToggle();
+    
     // Abrir lightbox
     document.querySelectorAll('.proyecto-item').forEach(item => {
         item.onclick = function(e) {
@@ -14,11 +18,36 @@ document.addEventListener('DOMContentLoaded', function() {
             this.parentElement.parentElement.style.display = 'none';
         }
     });
+
+    // Contenido de analytics.js
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'TU_ID_DE_SEGUIMIENTO');
+
 });
 
-// Contenido de analytics.js
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
+function initializeThemeToggle() {
+    console.log("Entrando en cambio de tema");
+    const toggleThemeButton = document.querySelector('#toggle-theme');
 
-gtag('config', 'TU_ID_DE_SEGUIMIENTO');
+    if(toggleThemeButton) {
+        toggleThemeButton.addEventListener('click', () => {
+            console.log("Itento de cambio de tema");
+            document.body.classList.toggle('dark-theme');
+            // Guardar la preferencia de tema en el almacenamiento local
+            if(document.body.classList.contains('dark-theme')) {
+                localStorage.setItem('theme', 'dark');
+            } else {
+                localStorage.removeItem('theme');
+            }
+        });
+    }
+
+    // Aplicar el tema según la preferencia guardada al cargar la página
+    if(localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+}
+
